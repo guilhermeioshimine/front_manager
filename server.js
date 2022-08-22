bbafc3ce8eaf4a1569260fdc1df75688ce695fd9
Guilherme <guilherme.ioshimine@gmail.com>
2022-08-22 00:25:29 -0300

    deploy

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const path = require('path');
-const frontend = process.env.npm_package_name;
 const app = express();
 
-app.use(express.static(`${__dirname}/dist/${frontend}`));
+app.use(express.static(`${__dirname}/dist/frontend`));
 
 app.get('/*', (req, res) => {
-res.sendFile(path.join(`${__dirname}/dist/${frontend}/index.html`));
+res.sendFile(path.join(`${__dirname}/dist/frontend/index.html`));
 });
 
 app.listen(process.env.PORT || 8080);
